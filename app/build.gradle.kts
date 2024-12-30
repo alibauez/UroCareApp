75ae2fbe583d6beacb31ebecf09352720d44e9b1
alibauez <148813647+alibauez@users.noreply.github.com>
2024-12-30 12:22:13 +0100

    calendar no funciona

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,10 +47,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+//    implementation(libs.material.calendarview)
+//    implementation("com.prolificinteractive:material-calendarview:1.7.0")
 
     implementation ("com.google.android.material:material:1.9.0")
 
 
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-auth-ktx")
+
+
+
 }
