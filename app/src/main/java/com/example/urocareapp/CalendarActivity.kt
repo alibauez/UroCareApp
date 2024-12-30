875124ef38a4fa6d55d1841e522710b484df9959
alibauez <148813647+alibauez@users.noreply.github.com>
2024-12-30 16:13:16 +0100

    calendar

--- app/src/main/java/com/example/urocareapp/CalendarActivity.kt
+++ app/src/main/java/com/example/urocareapp/CalendarActivity.kt
@@ -1,15 +1,26 @@
 package com.example.urocareapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.CalendarView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.urocareapp.R
 
 class CalendarActivity : AppCompatActivity() {
+
+    private lateinit var calendarView: CalendarView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
+        // Inicialización del CalendarView
+        calendarView = findViewById(R.id.calendarView)
+
+        // Listener para el cambio de fecha
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val date = "$dayOfMonth/${month + 1}/$year"
+            Toast.makeText(this@CalendarActivity, "Fecha seleccionada: $date", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/urocareapp/HomePaciente.kt
+++ app/src/main/java/com/example/urocareapp/HomePaciente.kt
@@ -7,21 +7,28 @@ import android.widget.Button
 class HomePaciente : BaseActivity() {
 
     private lateinit var btnPantallaInfo: Button
+    private lateinit var btnCalendar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_paciente)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-
-        btnPantallaInfo = findViewById(R.id.btnPantallaInfo) // Inicializa aquí
+        btnPantallaInfo = findViewById(R.id.btnPantallaInfo)
+        btnCalendar = findViewById(R.id.btnCalendar)
         setup()
     }
 
     fun setup(){
         val intentPantallaInfo = Intent(this, PantallaInfo::class.java)
+        val intentCalendar = Intent(this, CalendarActivity::class.java)
+
         btnPantallaInfo.setOnClickListener {
             startActivity(intentPantallaInfo)
+        }
+
+        btnCalendar.setOnClickListener {
+            startActivity(intentCalendar)
         }
     }
 }
