318619ef9b56d5f2a7fed34592e68f87514b0e90
A\alber <albertogmpv2502@gmail.com>
2025-01-02 14:29:31 +0100

    COMMIT

--- app/src/main/java/com/example/urocareapp/PerfilPaciente.kt
+++ app/src/main/java/com/example/urocareapp/PerfilPaciente.kt
@@ -2,7 +2,9 @@ package com.example.urocareapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
@@ -11,6 +13,8 @@ import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.userProfileChangeRequest
 import com.google.firebase.firestore.SetOptions
@@ -27,7 +31,7 @@ class PerfilPaciente : BaseActivity() {
 
 
         val userNameTextView = findViewById<TextView>(R.id.tvName)
-        val userAgeTextView = findViewById<TextView>(R.id.tvAge)
+
         val userGenderTextView = findViewById<TextView>(R.id.tvGender)
 
         val bloodGroupSpinner = findViewById<Spinner>(R.id.spinnerBloodGroup)
@@ -37,23 +41,43 @@ class PerfilPaciente : BaseActivity() {
 
         val etHeight = findViewById<EditText>(R.id.etHeight)
         val etWeight = findViewById<EditText>(R.id.etWeight)
-        val etAllergies = findViewById<EditText>(R.id.etAllergies)
+
         val email = Firebase.auth.currentUser?.email
+        val birthDateTextView = findViewById<TextView>(R.id.tvDate)
 
 
 
-        Firebase.firestore.collection("Pacientes")
+        Firebase.firestore.collection("pacientes")
             .document(email.toString())
             .get()
             .addOnSuccessListener {
                 var nombreBD = it.get("nombre")
                     .toString()//el campo debe ser igual al que se usa en la base de datos (mapOf)
                 userNameTextView.setText(if (nombreBD != "null") nombreBD else "")
-                var ageBD = it.get("edad").toString()
-                userAgeTextView.setText(if (ageBD != "null") ageBD else "")
                 var genderBd = it.get("genero").toString()
                 userGenderTextView.setText(if (genderBd != "null") genderBd else "")
+                var groupBD = it.get("grupoSanguineo").toString()
+                if (groupBD != "null") {
+                    val bloodGroupOptions = resources.getStringArray(R.array.blood_group_options)
+                    val position = bloodGroupOptions.indexOf(groupBD) // Encuentra la posición del valor en las opciones
+                    if (position >= 0) {
+                        bloodGroupSpinner.setSelection(position) // Establece el valor inicial del Spinner
+                    }
+                }
+                // Fecha de nacimiento
+                val birthDateTimestamp = it.getTimestamp("fechaNacimiento")
+                birthDateTimestamp?.let { timestamp ->
+                    val birthDate = timestamp.toDate()
+                    val dateFormat = java.text.SimpleDateFormat("dd/MM/yyyy", java.util.Locale.getDefault())
+                    val formattedDate = dateFormat.format(birthDate)
+
+                    birthDateTextView.text = formattedDate
+                }
+                val allergiesList = it.get("alergias") as? List<String> ?: emptyList()
+                Log.d("alergias", allergiesList.toString())
+                setupRecyclerView(allergiesList)
             }
+
 
 
 
@@ -63,16 +87,7 @@ class PerfilPaciente : BaseActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         bloodGroupSpinner.adapter = adapter
 
-        // Establecer el valor inicial (opcional, basado en Firebase)
 
-        val userBloodGroup = "0+" // Cambiar esto para cargar desde la base de datos o un campo específico
-        val initialPosition = bloodGroupOptions.indexOf(userBloodGroup)
-        if (initialPosition >= 0) {
-            bloodGroupSpinner.setSelection(initialPosition)
-        }
-
-        // Obtener el valor seleccionado cuando sea necesario
-        val selectedBloodGroup = bloodGroupSpinner.selectedItem.toString()
 
         bloodGroupSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
@@ -94,31 +109,17 @@ class PerfilPaciente : BaseActivity() {
             }
         }
 
-        // Cargar los datos del usuario desde Firestore
-        userId?.let {
-            db.collection("users").document(it).get().addOnSuccessListener { document ->
-                if (document != null) {
-                    // Cargar los datos en los campos
-                    etHeight.setText(document.getString("height") ?: "")
-                    etWeight.setText(document.getString("weight") ?: "")
-                    etAllergies.setText(document.getString("allergies") ?: "")
-                } else {
-                    Log.d("Firebase", "No se encontró el documento del usuario")
-                }
-            }.addOnFailureListener { exception ->
-                Log.w("Firebase", "Error al obtener datos: ", exception)
-            }
-        }
+
         val btnSave = findViewById<Button>(R.id.btnSave)
         btnSave.setOnClickListener {
             val updatedHeight = etHeight.text.toString()
             val updatedWeight = etWeight.text.toString()
-            val updatedAllergies = etAllergies.text.toString()
+
 
             val userData = hashMapOf(
                 "height" to updatedHeight,
                 "weight" to updatedWeight,
-                "allergies" to updatedAllergies
+
             )
 
             userId?.let {
@@ -131,11 +132,40 @@ class PerfilPaciente : BaseActivity() {
                         Log.w("Firebase", "Error al guardar datos: ", e)
                         Toast.makeText(this, "Error al guardar datos", Toast.LENGTH_SHORT).show()
                     }
+
+
+
+
             }
         }
+
+
     }
 
+    class AllergiesAdapter(private val allergies: List<String>) :
+        RecyclerView.Adapter<AllergiesAdapter.AllergyViewHolder>() {
 
+        inner class AllergyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val allergyTextView: TextView = itemView.findViewById(R.id.tvAllergy)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllergyViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_allergy, parent, false)
+            return AllergyViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: AllergyViewHolder, position: Int) {
+            holder.allergyTextView.text = allergies[position]
+        }
+
+        override fun getItemCount(): Int = allergies.size
+    }
+    fun setupRecyclerView(allergies: List<String>) {
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewAllergies)
+        recyclerView.layoutManager = LinearLayoutManager(this) // Asegúrate de configurar el layout manager
+        recyclerView.adapter = AllergiesAdapter(allergies)
+    }
 
 
 }
